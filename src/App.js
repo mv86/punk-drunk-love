f4aeb39647cb8284617317fb0d38a53257444106
Max <mveasey87@gmail.com>
2017-02-21 16:47:16 +0000

    added nav bar with ul

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ class App extends Component {
         <div className="App-header">
           <h1>You like Punk Ipa???</h1>
         </div>
+         <ul id="nav-ul">
+            <li>Home</li>
+            <li>Search Beers</li>
+            <li>View My Beers</li>
+          </ul>
         <div className="App-info">
           <h1 id="lower-h1">You'll love this site!</h1>
           <MainPage/>
--- src/AppInfo.js
+++ src/AppInfo.js
@@ -3,8 +3,10 @@ import React from 'react';
 const AppInfo = () => {
 		return (
 			<div id="app-description">
+				<h2>Welcome to <span className="pdl-name">Punk Drunk Love!</span></h2>
 				<p>Here is a lot of info about my app.
-				This is such an interesting site.</p>
+				This is such an interesting site, please explore. 
+				There is a random beer selection to the right >>></p>
 			</div>
 			)
 }
