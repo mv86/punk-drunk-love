c883a03635d258f3604614411c9f0776efa21c10
Max <mveasey87@gmail.com>
2017-02-24 15:00:23 +0000

    edited css & fixed nav-bar postiion

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h1>You like Punk Ipa???</h1>
+        <div className="header-wrapper">
+          <div className="App-header">
+            <h1 id="app-title">Punk Drunk Love</h1>
+            <ul id="nav-ul">
+              <li>Home</li>
+              <li>Search Beers</li>
+              <li>View My Beers</li>
+            </ul>
+          </div>
         </div>
-         <ul id="nav-ul">
-            <li>Home</li>
-            <li>Search Beers</li>
-            <li>View My Beers</li>
-          </ul>
         <div className="App-info">
-          <h1 id="lower-h1">You'll love this site!</h1>
+          <h1 id="lower-h1">Beautiful Brewdog Beers!</h1>
           <MainPage/>
         </div>
       </div>
